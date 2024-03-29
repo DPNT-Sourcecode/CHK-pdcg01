4c221a0ab991d7a9683b8bb65c0033055c26c28e
sbx_user1051 <sbx_user1051@169.254.85.197>
2024-03-29 10:16:56 +0000

    Fri Mar 29 10:16:56 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,27 @@
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+
 class CheckoutSolutionTest {
-  
+    private CheckoutSolution chk;
+
+    @BeforeEach
+    public void setUp() {
+        chk = new CheckoutSolution();
+    }
+
+    @Test
+    public void testCheckoutWithInvalidString() {
+        assertThat(chk.checkout("John"), equalTo(-1));
+    }
+
+    @Test
+    public void testCheckoutWithEmptyString() {
+        assertThat(chk.checkout(""), equalTo(0));
+    }
 }
